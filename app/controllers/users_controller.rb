515a0223bf34de38f2a50af4cba5946fc4aefbc4
Taehoon Yoon <thyoon515@gmail.com>
2023-05-11 19:18:08 -0400

    Add signup feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    before_action :authorize
+    skip_before_action :authorize, only: [:create]
+
     def index
         render json: User.all
     end
@@ -10,5 +13,23 @@ class UsersController < ApplicationController
         else
           render json: { error: "Not authorized" }, status: :unauthorized
         end
+    end
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    rescue ActiveRecord::RecordInvalid => e
+        render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    private
+
+    def user_params
+        params.permit(:first_name, :last_name, :password, :email)
+    end
+  
+    def authorize
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
     end
 end
--- client/src/components/sessions/Signup.js
+++ client/src/components/sessions/Signup.js
@@ -1,8 +1,153 @@
-import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
-const Signup = () => {
+const Signup = ({ setUserLoggedIn }) => {
+
+  const [signupUserInputs, setSignupUserInputs] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: ""
+  });
+  const [errors, setErrors] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleSignupSubmit = (e) => {
+    e.preventDefault();
+    fetch(`/users`, {
+      method: "POST",
+      headers:{'Content-Type':'application/json'},
+      body:JSON.stringify({
+        first_name: signupUserInputs.first_name,
+        last_name: signupUserInputs.last_name,
+        email: signupUserInputs.email,
+        password: signupUserInputs.password
+      })
+    })
+    .then(res => {
+      if(res.ok){
+          res.json().then(() => {
+            setUserLoggedIn(true)
+            //setCurrentUser(newUser)
+            navigate('/')
+          })
+      }else{
+          res.json().then((e) => {
+            setErrors(e.errors)
+          })
+      }
+    })
+  }
+
+  const handleUserInputChange = (e) => {
+    const key = e.target.id
+    setSignupUserInputs({
+      ...signupUserInputs,
+      [key]: e.target.value
+    })
+  }
+
   return (
-    <div>Signup</div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSignupSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="first_name"
+                label="First Name"
+                value={signupUserInputs.first_name}
+                onChange={handleUserInputChange}
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="last_name"
+                label="Last Name"
+                value={signupUserInputs.last_name}
+                onChange={handleUserInputChange}
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                value={signupUserInputs.email}
+                onChange={handleUserInputChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                id="password"
+                value={signupUserInputs.password}
+                onChange={handleUserInputChange}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <div>
+           {errors && (
+           <ul style={{ color: "red" }}>
+             {errors.map((error) => (
+               <li key={error}>{error}</li>
+            ))}
+           </ul>
+           )}
+          </div>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Login
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   )
 }
 
