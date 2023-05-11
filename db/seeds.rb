1dd7b47a4417038484b8db2a911786306f1386af
Taehoon Yoon <thyoon515@gmail.com>
2023-05-11 18:32:13 -0400

    Create components folder in src. Add sub folders and files.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(first_name: 'Tae', last_name: 'Yoon', password: '1234', email: 'Tae@email.com')
+User.create!(first_name: 'TJ', last_name: 'Max', password: '1234', email: 'TJ@gmail.com')
