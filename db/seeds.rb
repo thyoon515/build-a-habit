d54145b1a0dc7e81ab490049ba527501b148c8a2
Taehoon Yoon <thyoon515@gmail.com>
2023-06-10 12:57:53 -0400

    reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# User.create!(first_name: 'Tae', last_name: 'Yoon', password: '1234', email: 'Tae@email.com')
+User.create!(first_name: 'Tae', last_name: 'Yoon', password: '1234', email: 'Tae@email.com')
 # User.create!(first_name: 'TJ', last_name: 'Max', password: '1234', email: 'TJ@gmail.com')
 
 # Priority.create!(order: '1st', color: 'red')
