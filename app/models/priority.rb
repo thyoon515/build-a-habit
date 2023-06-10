b3ad265022556b26587f9d1d338efd78b71b7158
Taehoon Yoon <thyoon515@gmail.com>
2023-06-10 12:45:58 -0400

    db:trunacate_all

--- app/models/priority.rb
+++ app/models/priority.rb
@@ -1,12 +1,4 @@
 class Priority < ApplicationRecord
     has_many :tasks
     has_many :categories, through: :tasks
-
-    before_validation :set_color_from_priority, on: :create
-
-    private
-
-    def set_color_from_priority
-        self.color = priority.color if priority.present?
-    end
 end
